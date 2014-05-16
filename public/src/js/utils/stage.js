508745bad415649d3513858b47e6aca090b715ed
Paul Hoover <paul.hoover@selectablemedia.com>
2014-05-16 10:01:05 -0400

    test/index using src files, stage upgrades, phaser 2.0 added to lib/phaser

--- public/src/js/utils/stage.js
+++ public/src/js/utils/stage.js
@@ -1,14 +1,17 @@
 Polyworks.Stage = (function() {
 	var _aspectRatio = [16, 9];
 	var _windowListeners = false;
+	var _center;
 	var _callback;
 	var _context; 
 	
 	var module = {
 		winW: 0,
 		winH: 0,
-		stageW: 0,
-		stageH: 0,
+		gameW: 0,
+		gameH: 0,
+		gameX: 0,
+		gameY: 0,
 		unit: 0,
 
 		init: function(aspectRatio, resizable, callback, context) {
@@ -31,6 +34,9 @@ Polyworks.Stage = (function() {
 				});
 			}
 
+			if(_callback) {
+				_callback.call(_context);
+			}
 		},
 
 		destroy: function() {
@@ -49,45 +55,45 @@ Polyworks.Stage = (function() {
 		module.winW = document.documentElement.clientWidth; 
 		module.winH = document.documentElement.clientHeight;
 
-		module.height = (document.documentElement.clientHeight > 800) ? 800 : document.documentElement.clientHeight;
-		module.width = ((document.documentElement.clientHeight/_aspectRatio[1]) * _aspectRatio[0]);
+		module.gameH = (module.winH > 800) ? 800 : module.winH;
+		module.gameW = ((module.winH/_aspectRatio[1]) * _aspectRatio[0]);
 
-		if(module.width > document.documentElement.clientWidth) {
-			module.width = document.documentElement.clientWidth;
-			module.height = (module.width/_aspectRatio[0]) * _aspectRatio[1];
+		if(module.gameW > module.winW) {
+			module.gameW = module.winW;
+			module.gameH = (module.gameW/_aspectRatio[0]) * _aspectRatio[1];
 		}
 
-		module.unit = module.height/_aspectRatio[1];
-		var left = (document.documentElement.clientWidth/2) - (module.width/2);
-		var top = (document.documentElement.clientHeight/2) - (module.height/2);
+		module.unit = module.gameH/_aspectRatio[1];
+		module.gameX = (module.winW/2) - (module.gameW/2);
+		module.gameY = (module.winH/2) - (module.gameH/2);
 
-		trace('\nwinW = ' + module.winW + ', winH = ' + module.winH + '\nmodule.width = ' + module.width + ', module.height = ' + module.height + '\nunit = ' + module.unit + '\nleft = ' + left + ', top = ' + top);
+		trace('\nwinW = ' + module.winW + ', winH = ' + module.winH + '\ngameW = ' + module.gameW + ', gameH = ' + module.gameH + '\nunit = ' + module.unit + '\ngameX = ' + module.gameX + ', gameY = ' + module.gameY);
 
 		var loadingWidth = module.winW - 80;
 		var loadingHeight = module.winH - 80;
 
 		var loadingDiv = document.getElementById('loading');
-		var containerDiv = document.getElementById('gameContainer');
+		var containerDiv = document.getElementById('game_container');
 
-		_sizeAndPositionDiv(loadingDiv, loadingWidth, loadingHeight, 0, 0);
-		_sizeAndPositionDiv(containerDiv, module.winW, module.winH, 0, 0);
+		if(loadingDiv) _sizeAndPositionDiv(loadingDiv, loadingWidth, loadingHeight, 0, 0);
+		if(containerDiv) _sizeAndPositionDiv(containerDiv, module.gameW, module.gameH, module.gameX, module.gameY);
 
 	}
 
+
 	function _sizeAndPositionDiv(div, width, height, left, top) {
-		if(div) {
-			div.style.width = width + 'px';
-			div.style.height = height + 'px';
-			div.style.left = left + 'px';
-			div.style.top = top + 'px';
-			div.style.display = 'block';
-		}
+		trace('Stage/_sizeAndPositionDiv, div = ', div, '\twidth = ' + width + ', height = ' + height + '\n\tleft = ' + left + ', top = ' + top);
+		div.style.width = width + 'px';
+		div.style.height = height + 'px';
+		div.style.left = left + 'px';
+		div.style.top = top + 'px';
+		div.style.display = 'block';
 	}
 
 	function _onSizeChange(event) {
-		var containerDiv = document.getElementById('gameContainer');
-		var left = (document.documentElement.clientWidth/2) - (module.width/2);
-		var top = (document.documentElement.clientHeight/2) - (module.height/2);
+		var containerDiv = document.getElementById('game_container');
+		var left = (module.winW/2) - (module.gameW/2);
+		var top = (module.winH/2) - (module.gameH/2);
 		containerDiv.style.left = left + 'px';
 		containerDiv.style.top = top + 'px';
 	}
