2dc8ca329f213cf3657d51f38218575e7e7a847c
paulhoover.air <polyworksgames@gmail.com>
2014-05-25 15:50:09 -0400

    third_party/phaser moved to phaser; game_data.js added to test, begun to be formatted

---
+++ public/test/js/game_data.js
@@ -0,0 +1,45 @@
+var gameData = {
+	money: 1000000,
+	factories: [
+	{
+		location: 3
+	},
+	{
+		location: 10
+	}
+	],
+	showrooms: [
+	{
+		
+	},
+	{
+		
+	}
+	],
+	equipment: {
+		tractors: {
+			basic: [
+			],
+			medium: [
+			],
+			heavy: [
+			]
+		},
+		skidSteers: {
+			basic: [
+			],
+			medium: [
+			],
+			heavy: [
+			]
+		}
+	},
+	tradeRoutes: [
+	{
+		name: 'asia',
+		lifeTime: 2,
+		consumption: {
+			tractors: 
+		}
+	}]
+}
