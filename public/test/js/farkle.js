6de927cee4a94ecad09d41bbda292feb8c9860fd
Paul Hoover <paul.hoover@selectablemedia.com>
2014-08-05 14:54:25 -0400

    farkle: TurnDice functioning with post roll sorting, scoring dice removal and scores added to throws

--- public/test/js/farkle.js
+++ public/test/js/farkle.js
@@ -3,14 +3,162 @@ var Farkle = function() {
 	
 	var NUM_DICE = 6;
 	var MIN_TURN_SCORE = 300;
-	var FIVE_SCORE = 50;
-	var ONE_SCORE = 100;
-	var TRIPLE_MULTIPLIER = 100;
 	var STRAIGHT_SCORE = 1500;
 	var FULL_HOUSE_SCORE = 750;
+	var TRIPLE_MULTIPLIER = 100;
+	var FIVE_SCORE = 50;
+	var ONE_SCORE = 100;
 	var TRIPLE_FARKLE = 500;
 	var WINNING_SCORE = 10000;
 
+	// graphics
+	var images = {
+		dice: [
+			'assets/img/die_one.png',
+			'assets/img/die_two.png',
+			'assets/img/die_three.png',
+			'assets/img/die_four.png',
+			'assets/img/die_five.png',
+			'assets/img/die_six.png'
+		]
+	};
+	
+	function GUIDice() {
+		this.dice = [];
+	};
+	
+	GUIDice.prototype.add = function(die) {
+		this.dice.push(die);
+	};
+	
+	GUIDice.prototype.display = function() {
+		
+	};
+	
+	function GUIDie(idx, value) {
+		this.idx = idx;
+		this.value = value;
+		this.image.url = images.dice[value];
+	};
+	
+	// data / calculations
+	function TurnDice(startingDice) {
+		this.availableDice = startingDice;
+		this.activeRoll = [];
+		this.throwScores = [];
+	}
+	
+	TurnDice.prototype.setActiveRoll = function(roll) {
+		this.activeRoll = roll;
+		this.sortActive();
+	};
+	
+	TurnDice.prototype.sortActive = function() {
+		this.triples = {};
+		this.hotDice = false;
+		this.farkled = true;
+
+		if(Farkle.straightTest(this.activeRoll)) {
+			// STRAIGHT
+			this.hotDice = true;
+			this.farkled = false;
+		} else {
+			var counts = PWG.Utils.elementCount(this.activeRoll);
+			var doubles = [];
+			trace('\tcounts = ', counts);
+			for(var key in counts) {
+				if(counts[key] >= 3) {
+					this.triples[key] = counts[key];
+					this.farkled = false;
+				} else if(counts[key] === 2) {
+					doubles.push(key);
+				}
+			}
+			if(doubles.length === 3) {
+				// FULL HOUSE
+				this.hotDice = true;
+				this.farkled = false;
+			} else {
+				if(PWG.Utils.objLength(this.triples.length) > 0) {
+					this.farkled = false;
+					if(PWG.Utils.objLength(this.triples.length) === 2) {
+						this.hotDice = true;
+					}
+				}
+				// DON'T BOTHER WITH 1s AND 5s IF HOTDICE
+				if(!this.hotDice) {
+					if(counts[1] || counts[5]) {
+						this.farkled = false;
+					}
+				}
+			}
+		}
+		trace('post sort active, farkled = ' + this.farkled);
+	};
+	
+	TurnDice.prototype.bankScoringDice = function(selection) {
+		var scoringDice = [];
+
+		PWG.Utils.each(
+			selection, 
+			function(die) {
+				var idx = this.activeRoll.indexOf(die);
+				if(idx > -1) {
+					var scoring = this.activeRoll.splice(idx, 1);
+					scoringDice.push(scoring[0]);
+					this.availableDice--;
+				}
+			},
+			this
+		);
+		trace('TurnDice/bankScoringDice\n\tscoringDice now = ', scoringDice, '\tactive now = ', this.activeRoll, '\tavailable = ' + this.availableDice);
+
+		var score = 0;
+		if(Farkle.straightTest(scoringDice.sort())) {
+			score = STRAIGHT_SCORE;
+		} else {
+			var counts = PWG.Utils.elementCount(scoringDice);
+			var doubles = [];
+			var triples = {};
+
+			for(var key in counts) {
+				if(counts[key] >= 3) {
+					triples[key] = counts[key];
+					delete counts[key];
+				} else if(counts[key] === 2) {
+					doubles.push(key);
+				}
+			}
+
+			if(doubles.length === 3) {
+				score = FULL_HOUSE_SCORE;
+			} else {
+				if(PWG.Utils.objLength(triples) > 0) {
+					// trace('there are triples');
+					PWG.Utils.each(
+						triples,
+						function(triple, key) {
+							// trace('\tadding triples for '+key+': ' + triple);
+							var multipler = (key === 1) ? TRIPLE_MULTIPLIER * ONE_SCORE : TRIPLE_MULTIPLIER;
+							score += (key * multipler) * (triple/3);
+						},
+						this
+					);
+				}
+				if(counts[1] && counts[1] < 3) {
+					score += (counts[1] * ONE_SCORE);
+				}
+				if(counts[5] && counts[5] < 3) {
+					score += (counts[5] * FIVE_SCORE);
+				}
+			}
+		}
+		this.throwScores.push(score);
+		trace('\tthrowScores now: ' + this.throwScores);
+	};
+
+	module.TurnDice = TurnDice;
+	
 	module.straight = [1, 2, 3, 4, 5, 6];
 	
 	module.startGame = function() {
@@ -26,36 +174,42 @@ var Farkle = function() {
 
 	module.startTurn = function() {
 		this.turnScore = 0;
-		this.hotDice = false;
-		this.available = NUM_DICE;
+		this.turnDice = new Farkle.TurnDice(NUM_DICE);
 		this.startRoll();
 	};
 	
 	module.startRoll = function() {
-		var currentRoll = this.roll(this.available);
+		var currentRoll = this.roll(this.turnDice.availableDice);
 		trace('current roll = ' + currentRoll);
-		if(this.farkleTest(currentRoll)) { 
+		// this.displayRoll(currentRoll);
+		this.turnDice.setActiveRoll(currentRoll);
+
+		if(this.turnDice.farkle) {
 			trace('FARKLED! womp womp');
 			this.endTurn(true);
 		} else {
-			trace('----- turn score = ' + this.turnScore);
-			if(this.hotDice) {
-				this.hotDice = false;
-				var rollAgain = confirm('roll again?');
-				if(rollAgain) {
-					this.available = NUM_DICE;
-					this.startRoll();
-				} else {
-					this.endTurn(false);
-				}
-			} else {
-				// remove scoring dice
-				
-				if(this.turnScore > MIN_TURN_SCORE) {
-					
-				}
+			if(this.turnDice.score > MIN_TURN_SCORE) {
+				this.canEndTurn = true;
 			}
+			this.promptDiceToSave();
 		}
+	};
+	
+	module.displayRoll = function(roll) {
+		this.guiDice = new Farkle.GUIDice(); 
+		PWG.Utils.each(
+			roll,
+			function(die, idx) {
+				this.guiDice.add(new Farkle.GUIDie(idx, die));
+			},
+			this
+		);
+	};
+
+	module.promptDiceToSave = function() {
+		// have user select dice to score on
+		
+		// this.startRoll();
 	};
 	
 	module.endTurn = function(farkled) {
@@ -77,63 +231,6 @@ var Farkle = function() {
 			results.push(PWG.Utils.diceRoll());
 		}
 		return results.sort();
-	};
-	
-	module.farkleTest = function(rollResults) {
-		var farkled = true;
-		var onesAndFives = false; 
-		var triples = [];
-		var doubles = [];
-		var scoringDice = 0;
-
-		if(this.straightTest(rollResults)) {
-			this.hotDice = true;
-			farkled = false;
-			this.turnScore += STRAIGHT_SCORE;
-			trace('HOT DICE: straight!');
-		} else {
-			var counts = PWG.Utils.elementCount(rollResults);
-			trace('\tcounts = ', counts);
-			for(var key in counts) {
-				if(counts[key] >= 3) {
-					triples.push(key);
-					var multipler = (key === 1) ? TRIPLE_MULTIPLIER * 100 : TRIPLE_MULTIPLIER;
-
-					this.turnScore += (key * multipler) * (counts[key]/3);
-					farkled = false;
-				} else if(counts[key] === 2) {
-					doubles.push(key);
-				}
-			}
-			if(doubles.length === 3) {
-				this.hotDice = true;
-				farkled = false;
-				trace('HOT DICE: full house!');
-				this.turnScore += FULL_HOUSE_SCORE;
-			} else if(triples.length > 0){
-				farkled = false;
-				if(triples.length === 2) {
-					this.hotDice = true;
-					trace('HOT DICE: 2 triples');
-				} else {
-					trace('triples present');
-				}
-			}
-			if(!this.hotDice) {
-				if(counts.hasOwnProperty(1) || counts.hasOwnProperty(5)) {
-					if(counts[1] && !triples.hasOwnProperty(1)) {
-						this.turnScore += (counts[1] * ONE_SCORE);
-					}
-					if(counts[5] && !triples.hasOwnProperty(5)) {
-						this.turnScore += (counts[5] * FIVE_SCORE);
-					}
-					onesAndFives = true;
-					farkled = false;
-					trace('1s and/or 5s present');
-				}
-			}
-		}
-		return farkled;
 	};
 	
 	module.straightTest = function(arr) {
